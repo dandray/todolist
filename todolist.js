ca43e8ec6d7d5d8ef91c21caa7cfcdf169ccff08
dandray <dandray@MacBook-Air-de-Dan.local>
2016-08-04 10:00:57 +0200

    heroku port error

--- todolist.js
+++ todolist.js
@@ -45,4 +45,12 @@ on en crée une vide sous forme d'array avant la suite */
 	res.redirect('/todo');
 })
 
-app.listen(process.env.PORT || 5000)
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
